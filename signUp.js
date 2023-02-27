b6ab5f9516663ef71a0e79c8856d65bd3f4da74e
Susan Wangari <56164343+Susan-Wangari@users.noreply.github.com>
2023-02-27 14:23:01 +0300

    task management system with firebase authorization

--- signUp.js
+++ signUp.js
@@ -1,78 +1,85 @@
-// require('dotenv').config();
+// a function that enables you to initialize your project. Makes sure your project is ready
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+//connects the database
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
+  getAuth, //user authorization
+  createUserWithEmailAndPassword, //option for sign up with email and password
+  signInWithEmailAndPassword, //option for sign in with email and password
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
 
-  //configuration
+  //firebase configuration
   const firebaseConfig = {
-
+//used to connect the app
     apiKey: "AIzaSyDVbr9TSEr2SKExQNX6WCuAP3FWDe4hUh4",
-
+//domain used for authorization
     authDomain: "bootcamp-53597.firebaseapp.com",
 
     projectId: "bootcamp-53597",
 
     storageBucket: "bootcamp-53597.appspot.com",
-
+//for a messaging application
     messagingSenderId: "494575287387",
-
+//application id
     appId: "1:494575287387:web:965cc81fa650014f0a537e",
-
+//for creating google analytics
     measurementId: "G-56R4774WC9"
 
   };
 
-
+//initializing the application
 const app = initializeApp(firebaseConfig);
 // const database = getDatabase(app);
 const auth = getAuth();
-
+//login form
 const submitButton = document.getElementById("submit");
 const signupButton = document.getElementById("sign-up");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const main = document.getElementById("main");
 const createacct = document.getElementById("create-acct");
-
+//create account form
 const signupEmailIn = document.getElementById("email-signup");
 const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
 const signupPasswordIn = document.getElementById("password-signup");
 const confirmSignUpPasswordIn = document.getElementById(
   "confirm-password-signup"
 );
+//create account button
 const createacctbtn = document.getElementById("create-acct-btn");
-
+//return to login button
 const returnBtn = document.getElementById("return-btn");
-
+//declare variables
 var email,
   password,
-  signupEmail,
+  signupEmail, //assigned the input of the signupEmailIn
   signupPassword,
   confirmSignupEmail,
   confirmSignUpPassword;
-
+//when button is clicked validate that email entered matches confirm email and password matches too
+//if they match, create the account
 createacctbtn.addEventListener("click", function () {
   var isVerified = true;
 
+//assigns the value of the password input field to a variable signupEmail
   signupEmail = signupEmailIn.value;
+  //assigns the value of confirmSignupEmailIn and assigns it to confirmSignupEmail
   confirmSignupEmail = confirmSignupEmailIn.value;
-  if (signupEmail != confirmSignupEmail) {
+  if (signupEmail != confirmSignupEmail) { //makes sure the signUp email matches the signIn email.Useful in the case where the user enters the email without entering confirm email
     window.alert("Email fields do not match. Try again.");
+    //stops the user from logging in
     isVerified = false;
   }
-
+//assigns the value of the password input field to a variable
   signupPassword = signupPasswordIn.value;
+  //assigns the value of the confirm password input field to a variable
   confirmSignUpPassword = confirmSignUpPasswordIn.value;
   if (signupPassword != confirmSignUpPassword) {
     window.alert("Password fields do not match. Try again.");
     isVerified = false;
   }
-
+//makes sure every input field is filled
   if (
     signupEmail == null ||
     confirmSignupEmail == null ||
@@ -83,15 +90,22 @@ createacctbtn.addEventListener("click", function () {
     isVerified = false;
   }
 
-  if (isVerified) {
+  if (isVerified) { //makes sure everything is verified
+    /*calls the function createUserWithEmailAndPassword that was created
+      the auth,signUpEmail and signupPassword are used as parameters
+      It is a claaback function*/
     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+    //used for creating the next step after the user signs up
       .then((userCredential) => {
         // Signed in
         // const user = userCredential.user;
         // ...
+        //tells the user that the account was created successfully
         window.alert("Success! Account created.");
+        //directs the user to a different page. The location is a file/different web page
         window.location = "./createTask.html";
       })
+      //catches any other error not captured above
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
@@ -103,8 +117,10 @@ createacctbtn.addEventListener("click", function () {
 });
 
 submitButton.addEventListener("click", function () {
+  //assigns the input of the email field to the email variable
   email = emailInput.value;
   // console.log(email);
+   //assigns the input of the passowrd field to the password variable
   password = passwordInput.value;
   // console.log(password);
 
@@ -112,12 +128,14 @@ submitButton.addEventListener("click", function () {
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-
+      //tells the user they have successfully logged in
       window.alert("Success! Welcome back!");
+       //directs the user to a different page. The location is a file/different web page
       window.location = "./createTask.html";
 
       // ...
     })
+    //catches a different error not specified
     .catch((error) => {
       // const errorCode = error.code;
       // const errorMessage = error.message;
@@ -134,142 +152,3 @@ returnBtn.addEventListener("click", function () {
   main.style.display = "block";
   createacct.style.display = "none";
 });
-
-// //add firebase to our web app
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-// import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-// //import authorization
-// import {
-//     getAuth,
-//     createUserWithEmailAndPassword,
-//     signInWithEmailAndPassword,
-//   } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-
-//   //configuration
-//   const firebaseConfig = {
-//     apiKey: "AIzaSyDVbr9TSEr2SKExQNX6WCuAP3FWDe4hUh4",
-//     authDomain: "bootcamp-53597.firebaseapp.com",
-//     projectId: "bootcamp-53597",
-//     storageBucket: "bootcamp-53597.appspot.com",
-//     messagingSenderId: "494575287387",
-//     appId: "1:494575287387:web:965cc81fa650014f0a537e",
-//     measurementId: "G-56R4774WC9"
-//   };
-
-//   //initialize firebase
-//   const app = initializeApp(firebaseConfig);
-// //calling the getAuth that was imported
-//   const auth = getAuth() 
-
-//   //login fields 
-// let emailInput = document.getElementById('email');
-// let passwordInput = document.getElementById('password');
-// let submitButton = document.getElementById('submit');
-
-// //create account
-// let signUpEmailIn = document.getElementById('email-signup');
-// let confirmSignUpEmailIn = document.getElementById('confirm-email-signup');
-// let signUpPasswordIn = document.getElementById('password-signup');
-// let confirmSignUpPasswordIn = document.getElementById('confirm-password-signup')
-// let  createacctbtn = document.getElementById('create-acct-btn');
-
-// //sign in as a form
-// let main = document.getElementById('main');
-// //create account/sign up as a form
-// let createAcct = document.getElementById('create-acct');
-
-// //signUp button
-// let signUpButton = document.getElementById('sign-up');
-// //return to login button
-// let returnButton = document.getElementById('return-btn');
-
-// var email,
-//   password,
-//   signupEmail,
-//   signupPassword,
-//   confirmSignupEmail,
-//   confirmSignUpPassword;
-
-// createacctbtn.addEventListener("click", function () {
-//   var isVerified = true;
-
-//   signupEmail = signupEmailIn.value;
-//   confirmSignupEmail = confirmSignupEmailIn.value;
-//   if (signupEmail != confirmSignupEmail) {
-//     window.alert("Email fields do not match. Try again.");
-//     isVerified = false;
-//   }
-
-//   signupPassword = signupPasswordIn.value;
-//   confirmSignUpPassword = confirmSignUpPasswordIn.value;
-//   if (signupPassword != confirmSignUpPassword) {
-//     window.alert("Password fields do not match. Try again.");
-//     isVerified = false;
-//   }
-
-//   if (
-//     signupEmail == null ||
-//     confirmSignupEmail == null ||
-//     signupPassword == null ||
-//     confirmSignUpPassword == null
-//   ) {
-//     window.alert("Please fill out all required fields.");
-//     isVerified = false;
-//   }
-
-//   if (isVerified) {
-//     createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
-//       .then((userCredential) => {
-//         // Signed in
-//         // const user = userCredential.user;
-//         // ...
-//         window.alert("Success! Account created.");
-//         window.location = "./createTask.html";
-//       })
-//       .catch((error) => {
-//         const errorCode = error.code;
-//         const errorMessage = error.message;
-//         // ..
-//         window.alert("Error occurred. Try again.");
-//         window.alert(errorMessage);
-//       });
-//   }
-// });
-
-// submitButton.addEventListener("click", function () {
-//   email = emailInput.value;
-//   // console.log(email);
-//   password = passwordInput.value;
-//   // console.log(password);
-
-//   signInWithEmailAndPassword(auth, email, password)
-//     .then((userCredential) => {
-//       // Signed in
-//       const user = userCredential.user;
-
-//       window.alert("Success! Welcome back!");
-//       window.location = "./createTask.html";
-
-//       // ...
-//     })
-//     .catch((error) => {
-//       // const errorCode = error.code;
-//       // const errorMessage = error.message;
-//       window.alert("Error occurred. Try again.");
-//     });
-// });
-
-// //set the login to be hidden when the signup button is clicked
-// signUpButton.addEventListener("click",function(){
-//     //sign in form becomes hidden
-//     main.style.display = 'none';
-//     //the create account form is displayed
-//     createAcct.style.display = "block";
-// }) 
-
-// returnButton.addEventListener("click",function(){
-//     //create account form becomes hidden
-//     createAcct.style.display = 'none';
-//     //the sign in form is displayed
-//     main.style.display = "block";
-// }) 
